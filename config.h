b26d2e8645c7a674af713eabb55048c817d92206
HarlowB1022 <brharlow@email.wm.edu>
2021-09-08 09:07:25 -0400

    Initial Commit

--- config.h
+++ config.h
@@ -1,24 +1,26 @@
 /* See LICENSE file for copyright and license details. */
+#include "X11/XF86keysym.h"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 8;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha = 0xd0;
+static const char col_gray1[]       = "#131532";
+static const char col_gray2[]       = "#0a0b1e";
+static const char col_gray3[]       = "#03040f";
+static const char col_gray4[]       = "#050614";
+static const char col_cyan[]        = "#74204e";
+static const unsigned int baralpha = 0x88;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { "#ff4d6d", col_gray1, NULL },
+	[SchemeSel]  = { "#ff4d6d", col_gray1,  NULL  },
 };
+
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
@@ -26,18 +28,18 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "", };
 
 static const char *tagsel[][2] = {
-	{ "#ffffff", "#ff0000" },
-	{ "#ffffff", "#ff7f00" },
-	{ "#000000", "#ffff00" },
-	{ "#000000", "#00ff00" },
-	{ "#ffffff", "#0000ff" },
-	{ "#ffffff", "#4b0082" },
-	{ "#ffffff", "#9400d3" },
-	{ "#000000", "#ffffff" },
-	{ "#ffffff", "#000000" },
+	{ "#ffffff", "#050614" },
+	{ "#ffffff", "#0a0b1e" },
+	{ "#ffffff", "#0f1028" },
+	{ "#ffffff", "#1c2f46" },
+	{ "#ffffff", "#25285a" },
+	{ "#ffffff", "#212455" },
+	{ "#ffffff", "#4b2252" },
+	{ "#ffffff", "#74204e" },
+	{ "#ffffff", "#9e1e4a" },
 };
 
 static const unsigned int tagalpha[] = { OPAQUE, baralpha };
@@ -66,7 +68,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -78,14 +80,37 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", "#ffffff", "-sb", col_cyan, "-sf", "#ffffff", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *brave[]    = { "brave", NULL };
+static const char *pcmanfm[]  = { "pcmanfm", NULL };
+static const char *spotify[]  = { "spotify", NULL };
+static const char *discord[]  = { "discord", NULL };
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
+static const char *brightUp[] = {  "~/dwm/bright +", NULL };
+static const char *brightDown[] = {  "~/dwm/bright -", NULL };
+static const char *flameshot[] = { "flameshot", "gui" };
 
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+ 	{ 0,                            XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+	{ 0,                            XF86XK_AudioMute, spawn, {.v = mutevol } },
+	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
+ 	{ 0,                            XF86XK_KbdBrightnessUp, spawn, {.v = brightUp } },
+ 	{ 0,                            XF86XK_KbdBrightnessDown, spawn, {.v = brightDown } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
+	{ MODKEY| ShiftMask,            XK_f,      spawn,          {.v = flameshot } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ Mod1Mask,                     XK_b,      spawn,          {.v = brave  } },
+	{ Mod1Mask,                     XK_f,      spawn,          {.v = pcmanfm } },
+	{ Mod1Mask,                     XK_m,      spawn,          {.v = spotify } },
+	{ Mod1Mask,                     XK_d,      spawn,          {.v = discord } },
+	{ MODKEY,                       XK_F11,    spawn,          {.v = downvol } },
+	{ MODKEY,                       XK_F9,     spawn,          {.v = mutevol } },
+	{ MODKEY,                       XK_F12,    spawn,          {.v = upvol   } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -97,7 +122,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
